c25c60f18730e1e41afdbd8b654649ec2398b2da
VigneshMadhavan <vigneshmadhavan@yahoo.co.in>
2018-04-17 22:42:17 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+console.log("Using for loop");
+
+for(var i=5;i<=50;i=i+5){
+	
+	console.log(i);
+	
+}
+
+console.log("Using while loop");
+var count=5;
+var increment=5;
+while(count<=50){
+	
+	console.log(count);
+	count+=increment;
+}
+
